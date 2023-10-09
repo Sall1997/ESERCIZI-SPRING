e6aa7008629d02378dfd66016619c362d72db181
Sall <salvatoreparisi97@gmail.com>
2023-10-09 18:49:43 +0200

    esercizio 3 parte 1 svolto.

--- Esercizio_Spring_Di_Greg/src/main/java/com/example/springboot/controller/NewMealController.java
+++ Esercizio_Spring_Di_Greg/src/main/java/com/example/springboot/controller/NewMealController.java
@@ -1,14 +1,14 @@
 package com.example.springboot.controller;
 
 import com.example.springboot.entity.Meal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class NewMealController {
+    List<Meal>meals= new ArrayList<>();
 
     @GetMapping("/meals")
     public List<Meal>getMeals(){
@@ -17,5 +17,12 @@ public class NewMealController {
         meals.add(new Meal("pasta col pomodoro", "pasta con pomodoro fresco",6, true));
         meals.add(new Meal("pasta con patate", "pasta con patate e salsiccia",6, true));
         return meals;
+    }
+
+    @PostMapping("/meal")
+    public Meal addMeal(@RequestBody Meal meal){
+        this.meals.add(meal);
+
+        return meal;
     }
 }
