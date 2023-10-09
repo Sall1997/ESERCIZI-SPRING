bac02f964b30a2c31da4c235b0c041e5e6b47513
Sall <salvatoreparisi97@gmail.com>
2023-10-09 17:08:00 +0200

    esercizio 1 parte 1 svolto.

--- Esercizio_Spring_Di_Greg/src/main/java/com/example/springboot/controller/HelloController.java
+++ Esercizio_Spring_Di_Greg/src/main/java/com/example/springboot/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.example.springboot.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HelloController {
 	@GetMapping(value = "/good-morning")
 	public ResponseEntity<String> index() {
 		return ResponseEntity.ok("Good morning!");
+	}
+
+	@GetMapping(value = "/hello")
+	public  ResponseEntity<String> helloWorld(){
+		return ResponseEntity.ok("Hello World");
 	}
 }
