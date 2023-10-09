45dbd82eb56ad92486fc4dedc15747e0d5409a1f
Sall <salvatoreparisi97@gmail.com>
2023-10-09 18:27:18 +0200

    esercizio 2 parte 2 svolto.

--- Esercizio_Spring_Di_Greg/src/main/java/com/example/springboot/controller/FindMealByName.java
+++ Esercizio_Spring_Di_Greg/src/main/java/com/example/springboot/controller/FindMealByName.java
@@ -2,20 +2,27 @@ package com.example.springboot.controller;
 
 import com.example.springboot.entity.Meal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class NewMealController {
+public class FindMealByName {
 
-    @GetMapping("/meals")
-    public List<Meal>getMeals(){
+    @GetMapping("/meal/{name}")
+    public List<Meal> findMealByName(@PathVariable String name){
         List<Meal>meals = new ArrayList<>();
+        List<Meal>mealsByName = new ArrayList<>();
         meals.add(new Meal("pasta col pesto", "pasta con pesto di basilico fresco",6, true));
         meals.add(new Meal("pasta col pomodoro", "pasta con pomodoro fresco",6, true));
         meals.add(new Meal("pasta con patate", "pasta con patate e salsiccia",6, true));
-        return meals;
+        for (Meal meal : meals) {
+            if(name.contains(meal.getName())){
+                mealsByName.add(meal);
+            }
+        }
+        return mealsByName;
     }
 }
